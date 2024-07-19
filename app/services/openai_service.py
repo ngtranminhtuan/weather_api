bf5fa017267b53b8002efe8c08375b3018f644a1
ngtranminhtuan <ngtranminhtuan@gmail.com>
2024-07-19 15:33:37 +0900

    Fixed wrong input

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import json
 from typing import List, Dict, Any
 from app.config.settings import Settings
+from fastapi import HTTPException
 
 class OpenAIService:
     def __init__(self, settings: Settings):
@@ -14,9 +15,18 @@ class OpenAIService:
             tools=tools,
             tool_choice="auto"
         )
-        arguments = response.choices[0].message.tool_calls[0].function.arguments
-        data_json = json.loads(arguments)
-        return data_json
+
+        # Check if the finish_reason is 'tool_calls'
+        if response.choices and response.choices[0].finish_reason == 'tool_calls':
+            arguments = response.choices[0].message.tool_calls[0].function.arguments
+            data_json = json.loads(arguments)
+            return data_json
+        else:
+            # If finish_reason is not 'tool_calls', return the assistant's message
+            print(response.choices[0].message)
+            assistant_message = response.choices[0].message.content
+            raise HTTPException(status_code=400, detail={"error": "No tool calls detected.", "message": assistant_message})
+
 
     async def generate_human_readable_response(self, location: str, weather_data: Dict[str, Any]) -> str:
         messages = [
